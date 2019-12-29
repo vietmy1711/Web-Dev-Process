a24de5e4422f87b058df8beef1c2eb99e3c2fac7
mm <zzvietmyzz@gmail.com>
2019-12-29 19:19:57 +0700

    Add event listener

---
+++ Drum Kit/index.js
@@ -0,0 +1,7 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++)
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    this.style.color = "white";
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+  });
