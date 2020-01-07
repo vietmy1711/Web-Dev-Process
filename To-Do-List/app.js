3a02782564648ea3a329f115fa4dd04509a899e6
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-01-07 20:03:18 +0700

    Finish To Do List

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -1,30 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -39,10 +33,8 @@ app.get("/work", function(req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.post("/work", function(req, res) {
-  let item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function() {
